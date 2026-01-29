f689673d276f8d37daa537a4bcbcbb8f7992e88e
mikemetic <48034935+mikemetic@users.noreply.github.com>
2026-01-29 13:31:32 -0500

    Add files via upload

---
+++ PairDashLatest/PairDashLatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairDashLatestApp.swift
+//  PairDashLatest
+//
+//  Created by Michael Williams on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PairDashLatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
