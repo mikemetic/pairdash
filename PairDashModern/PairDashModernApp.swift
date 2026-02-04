6bc310d41c62172818749b2e7bbca51e3cf0ad7d
mikemetic <48034935+mikemetic@users.noreply.github.com>
2026-02-04 13:33:46 -0500

    Add files via upload

---
+++ PairDashModern/PairDashModernApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairDashModernApp.swift
+//  PairDashModern
+//
+//  Created by Michael Williams on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PairDashModernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PairDashModern/SetupView.swift
@@ -0,0 +1,150 @@
+//
+//  SetupView.swift
+//  PairDash_New
+//
+//  Created by Michael Williams on 1/28/26.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    
+    @State private var control = "Pinch"
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image(.background)
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                }
+                VStack {
+                    Text("Theme")
+                        .font(.system(size: 60))
+                        .fontDesign(Font.Design.rounded)
+                        .bold()
+                        .foregroundColor(.white)
+                    HStack {
+                        VStack {
+                            Image(.primarycolors)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .padding(-20)
+                            Text("Primary")
+                                .font(.system(size: 20))
+                                .fontDesign(Font.Design.rounded)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(20)
+                        }
+                        VStack {
+                            Image(.blackandwhite)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .padding(-20)
+                            Text("B&W")
+                                .font(.system(size: 20))
+                                .fontDesign(Font.Design.rounded)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(20)
+                        }
+                        VStack {
+                            Image(.monochromatic)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .padding(-20)
+                            Text("Mono")
+                                .font(.system(size: 20))
+                                .fontDesign(Font.Design.rounded)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(20)
+                        }
+                    }
+                    Spacer()
+                        .frame(height: 80).frame(height:30)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Shapes")
+                            .font(.system(size: 60))
+                            .fontDesign(Font.Design.rounded)
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    HStack {
+                        VStack {
+                            Image(.star)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .padding(-20)
+                            Text("Basic Shapes")
+                                .font(.system(size: 20))
+                                .fontDesign(Font.Design.rounded)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(10)
+                        }
+                        VStack {
+                            Image(.element)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .padding(-20)
+                            Text("Elements")
+                                .font(.system(size: 20))
+                                .fontDesign(Font.Design.rounded)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(10)
+                        }
+                    }
+                    Spacer()
+                        .frame(height: 80).frame(height:30)
+                    
+                    VStack {
+                        Text("Control")
+                            .font(.system(size: 60))
+                            .fontDesign(Font.Design.rounded)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(10)
+                        Picker("Choose an option", selection: $control) {
+                            Text("Pinch").tag("Pinch")
+                            Text("Flick").tag("Flick")
+                            Text("Tap").tag("Tap")
+                        }
+                        .padding(10)
+                        
+                        .pickerStyle(.automatic)
+//                        Spacer()
+//                            .frame(height: 80).frame(height:30)
+                    }
+                    NavigationLink (destination: ContentView())
+                    {
+                        Image(.readybutton)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 125, height: 100)
+                            .cornerRadius(10)
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
+
+#Preview
+{
+    SetupView()
+    
+}
